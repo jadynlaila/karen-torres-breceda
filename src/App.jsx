0cd80d5cc4abcc8b8a79d9e7003d6770363f8cf2
Jadyn Calhoun <jlc2243@nau.edu>
2025-05-21 17:09:11 -0700

    responsive map image for invitation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from "react" 
+import React from "react"
 import Gallery from "./Gallery.jsx"
 import Invite from "./Invite.jsx"
+import About from "./About.jsx"
+import Contact from "./Contact.jsx"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="app">
-      <Invite />
-      {/* <Gallery /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Invite />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/Invite.jsx
+++ src/Invite.jsx
@@ -1,11 +1,69 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './Invite.css'
+import { Link } from "react-router-dom"
+import ImageMapper from 'react-img-mapper'
+import { useRef } from "react";
 
 function Invite() {
+    const containerRef = useRef(null); 
+    const [parentWidth, setParentWidth] = useState(0);
+
+    const mapAreas = [{
+        "id": "1",
+        "title": "About",
+        "shape": "square",
+        "name": "1",
+        "href": "/about",
+        "coords": [
+            51,517,153,550
+        ],
+      },
+      {
+        "id": "2",
+        "title": "Gallery",
+        "shape": "square",
+        "name": "2",
+        "href": "/gallery",
+        "coords": [
+            228,514,341,551
+        ],
+      },
+      {
+        "id": "3",
+        "title": "Contact",
+        "shape": "square",
+        "name": "3",
+        "href": "/contact",
+        "coords": [
+            381,515,509,549
+        ],
+      }
+    ];
+
+    useEffect(() => {
+        const updateWidth = () => {
+            if (containerRef.current) {
+                setParentWidth(containerRef.current.offsetWidth);
+            }
+        }
+        updateWidth();
+        const resizeObserver = new ResizeObserver(() => {
+            updateWidth();
+        });
+        if (containerRef.current) {
+            resizeObserver.observe(containerRef.current);
+        }
+        return () => {
+            resizeObserver.disconnect();
+        }
+    }, []);
+
     return (
-        <div className="invite-container">
-            <div className="invite">
-                <h1>hiiiiiii </h1>
+        <div className="invite-container-container">
+            <div className="invite-container" ref={containerRef}>
+                {parentWidth > 0 && (
+                <ImageMapper src="invitationn.png" name="invitation-map" className="invite-image" areas={mapAreas} responsive={true} parentWidth={parentWidth}/>
+                )}
             </div>
         </div>
     )
